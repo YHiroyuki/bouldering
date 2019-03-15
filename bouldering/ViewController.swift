7d4249518322582f27b223f73de2fe6ee1274afe
yamawaki hiroyuki <yamawaki@wonderpla.net>
2019-03-15 22:34:18 +0900

    Initial Commit

---
+++ bouldering/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bouldering
+//
+//  Created by 山脇寛之 on 2019/03/15.
+//  Copyright © 2019 Hiroyuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
